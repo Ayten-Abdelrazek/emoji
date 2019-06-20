d1f1b44d79c59c8eba6a8fb300f4620f5541ec55
Apple <apple@Apples-MacBook-Pro-108.local>
2019-06-20 16:35:54 -0400

    iteration 2

--- emoji/ViewController.swift
+++ emoji/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        
+       let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[0] 
         let alertController = UIAlertController(title: "How are you feeling today?", message: "", preferredStyle: UIAlertController.Style.alert)
+        
+        let emojis = ["😄": "Don't let the silly little things steal your happiness.", "😢": "A million words would not bring you back, I know because I tried, neither would a million tears, I know because I cried", "😷": "Hope you get well soon", "😡": "Count with me: 1..2..3..4..5..do you feel better now?", "🤩": "you are a superstar!", "😜": "a day without laughter is a day waisted"]
         
         
          alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
